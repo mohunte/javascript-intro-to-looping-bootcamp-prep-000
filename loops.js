7767551e129ba354966b719915d710eeb26b2de9
Monique Hunte <monhunte@gmail.com>
2018-10-15 19:08:11 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,20 @@
+function forLoop(array){
+  for(let i = 0; i<25; i++){
+    if(i===1){
+      array.push('I am 1 strange loop.');
+    } else {
+      array.push('I am ${i} strange loops.');
+    }
+  }
+  return array;
+}
+
+function whileLoop(n){
+  let countdown = 62;
+  
+  while (countdown>0){
+    console.log(--countdown);
+  }
+  return "done";
+}
+
